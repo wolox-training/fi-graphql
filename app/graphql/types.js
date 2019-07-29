c9e48f65cee1331ee1a229e16b5b8be09a9924e2
Francisco Iglesias <franciscoj.iglesias@hotmail.com>
2019-07-29 19:00:26 -0300

    Deprecate some fields

--- app/graphql/types.js
+++ app/graphql/types.js
@@ -6,9 +6,9 @@ module.exports = gql`
   type Mutation
   type Subscription
   type User {
-    firstName: String @deprecated(reason: "Use name field.")
-    name: String!
-    lastName: String!
+    firstName: String @deprecated(reason: "Deprecated field.")
+    name: String @deprecated(reason: "Deprecated field.")
+    lastName: String @deprecated(reason: "Deprecated field")
     username: String!
     email: String!
     password: String!
