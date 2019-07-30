6e13c93330b19546b1be570dc54369422c10e24a
Francisco Iglesias <franciscoj.iglesias@hotmail.com>
2019-07-30 19:19:34 -0300

    Added compare function

--- app/services/crypt.js
+++ app/services/crypt.js
@@ -2,3 +2,5 @@ const bcrypt = require('bcrypt'),
   config = require('../../config');
 
 exports.encrypt = password => bcrypt.hash(password, config.common.salt, (err, hash) => hash);
+
+exports.compare = (password, hashedPassword) => bcrypt.compare(password, hashedPassword, (err, res) => res);
