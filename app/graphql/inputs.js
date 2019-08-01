52b4b5dd15cee8f47d82eaea35913600b2b88a55
Francisco Iglesias <franciscoj.iglesias@hotmail.com>
2019-08-01 13:59:29 -0300

    Added more declarative deprecation message

--- app/graphql/inputs.js
+++ app/graphql/inputs.js
@@ -2,9 +2,11 @@ const { gql } = require('apollo-server');
 
 module.exports = gql`
   input UserInput {
-    firstName: String @deprecated(reason: "Deprecated field.")
-    name: String @deprecated(reason: "Deprecated field.")
-    lastName: String @deprecated(reason: "Deprecated field.")
+    firstName: String
+      @deprecated(reason: "Deprecated field, now the user no longer has firstName or lastName.")
+    name: String @deprecated(reason: "Deprecated field, now the user no longer has firstName or lastName.")
+    lastName: String
+      @deprecated(reason: "Deprecated field, now the user no longer has firstName or lastName.")
     email: String!
     password: String!
   }
